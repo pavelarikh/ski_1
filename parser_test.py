dc6f29e2abcf59452e110a36a69697f6f91fe2f6
pavelarikh <157211004+pavelarikh@users.noreply.github.com>
2024-01-28 17:05:12 +0300

    Update parser_test.py

--- parser_test.py
+++ parser_test.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 
+import const
+
 def openFile(path):
     with open(path) as file:
         src = file.read()
@@ -13,28 +15,26 @@ def getSoup(path):
     file = openFile(path)
     soup = createSoup(file)
     return soup
+
     
 def getItems(soup):
     rows = soup.find_all('tr', class_='table-responsive__row')
     
     result = []
-
     for item in rows:
         try:
             rank = item.select('td:nth-child(2)')[0].text.strip()
             name = item.find('span', class_='table-item__name').text
             team = item.find('td', class_='_team').find('a').text
-
             obj = {
                 'rank': rank,
                 'name': name,
                 'team': team,
             }
-
             result.append(obj)
         except:
             pass
-    
+
     return result
 
 def get_by_name(name, results):
@@ -43,13 +43,14 @@ def get_by_name(name, results):
     for result in results:
         if result['name'] == name:
             finded.append(result)
-    
+
     return finded
 
 def main():
-    soup = getSoup('./html/file1.html')
+    soup = getSoup(path_to_html24)
+    getItems(soup)
     result = getItems(soup)
-    print(get_by_name('Полина Милузова', result))
+    print(get_by_name('Александр Большунов', result))
 
 
 if __name__ == "__main__": 
