524da74e3e9bacbee9a5b36f8f92c676cd00b42f
Fyodor Sologub <141565918+FyodorSologub@users.noreply.github.com>
2024-01-29 17:00:24 +0300

    Update parser_test.py

--- parser_test.py
+++ parser_test.py
@@ -1,51 +1,7 @@
 import const
 from bs4 import BeautifulSoup
 from utils import openFile, createSoup, getSoup
-
-def getItems(soup):
-    rows = soup.find_all('tr', class_='table-responsive__row')
-    
-    result = []
-    for item in rows:
-        try:
-            rank = item.select('td:nth-child(2)')[0].text.strip()
-            name = item.find('span', class_='table-item__name').text
-            team = item.find('td', class_='_team').find('a').text
-            result_ = item.select('td:nth-child(5)')[0].text.strip()
-            gap = item.select('td:nth-child(6)')[0].text.strip()
-            compition = item.find('div', class_='match-info__game').text
-            
-            obj = {
-                'rank': rank,
-                'name': name,
-                'team': team,
-                'result_': result_,
-                'gap': gap,
-                'compition': compition,
-            }
-            result.append(obj)
-        except:
-            pass
-
-    return result
-
-def get_by_name(name, results):
-    finded = []
-
-    for result in results:
-        if result['name'] == name:
-             finded.append(result)
-
-    return finded
-
-def get_by_team(team, results):
-    finded = []
-
-    for result in results:
-        if result['team'] == team:
-             finded.append(result)
-
-    return finded
+from requests import getItems
 
 def main(path, name):
     soup = getSoup(path)
