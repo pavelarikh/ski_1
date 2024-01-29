6520c1fbe8480d8f7fc387f8e422c13b1e3130c3
Fyodor Sologub <141565918+FyodorSologub@users.noreply.github.com>
2024-01-29 16:59:28 +0300

    Create requests.py

---
+++ requests.py
@@ -0,0 +1,26 @@
+def getItems(soup):
+    rows = soup.find_all('tr', class_='table-responsive__row')
+    
+    result = []
+    for item in rows:
+        try:
+            rank = item.select('td:nth-child(2)')[0].text.strip()
+            name = item.find('span', class_='table-item__name').text
+            team = item.find('td', class_='_team').find('a').text
+            result_ = item.select('td:nth-child(5)')[0].text.strip()
+            gap = item.select('td:nth-child(6)')[0].text.strip()
+            compition = item.find('div', class_='match-info__game').text
+            
+            obj = {
+                'rank': rank,
+                'name': name,
+                'team': team,
+                'result_': result_,
+                'gap': gap,
+                'compition': compition,
+            }
+            result.append(obj)
+        except:
+            pass
+
+    return result
